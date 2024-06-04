40276020d890da49dbb211415548a7e2ba2e521c
Rizky Yanuar Ramadhan <yanuarrz71@gmail.com>
2024-06-04 23:40:52 +0700

    Add files via upload

--- task3.js
+++ task3.js
@@ -1,5 +1,25 @@
-const nilaiAwal = []
-const nilaiAkhir = []
-const dataArray = []
+function seleksiNilai(nilaiAwal, nilaiAkhir, dataArray){
+    if (nilaiAwal < nilaiAkhir) {
+        if (dataArray.length > 5) {
+            let hasil = []
+            for (let i = 0; i < dataArray.length; i++) {
+                if (dataArray[i] > nilaiAwal && dataArray[i] < nilaiAkhir){
+                    hasil.push(dataArray[i])
+                    hasil.sort((a,b) => a-b)
+                }
+            }if(hasil.length == 0){
+                return "Nilai tidak ada"
+            }
+            return hasil;
+            // console.log(temp)
+        }
+        else{
+            return "Jumlah angka dalam data array harus lebih dari 5"
+        }
+    }else {
+        return ("Nilai akhir harus lebih besar dari nilai awal")
+    }
 
-function seleksiNilai()
+}
+const hasil = seleksiNilai(50,20,[2,25,4,14,17,30,8])
+console.log(hasil)
