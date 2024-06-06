38674b53e78d9ce4ee042eabddd9944da3038997
Rizky Yanuar Ramadhan <yanuarrz71@gmail.com>
2024-06-06 19:11:42 +0700

    Add files via upload

--- task2.js
+++ task2.js
@@ -1,11 +1,68 @@
-const name = ['Abigail','Alexandra','Alison','Amanda',
-    'Angela','Bella','Carol','Caroline',
-    'Carolyn','Deirdre','Diana','Elizabeth',
-    'Ella','Faith','Olivia','Penelope',]
+function searchName(keyword , limit, cb){
 
-function searchName(name, callback){
-    return ('an', 3, callback)
+const name = [
+    'Abigail','Alexandra','Alison',
+    'Amanda','Angela','Bella',
+    'Carol','Caroline','Carolyn',
+    'Deirdre','Diana','Elizabeth',
+    'Ella','Faith','Olivia','Penelope'
+];
+
+
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const lowerCase = "abcdefghijklmnopqrstuvwxyz"
+let searchResult = []
+
+for(let i = 0; i < name.length; i++){
+    const personName = name[i]
+    let result = ""
+
+    for(let i = 0; i < personName.length; i++){
+        let converted = false
+        for (let j = 0; j < upperCase.length; j++) {
+            if(converted === false){
+                if (personName[i] === upperCase[j]) {
+                    result += lowerCase[j]
+                    converted = true
+                }
+                if(j === 25){
+                    if(personName[i] !== upperCase[j]){
+                        result += personName[i]
+                        converted = true
+                    }
+                }
+            }
+        }
+    }
+
+    let isFound = false
+    for (let i = 0; i < result.length; i++) {
+        let segment = ""
+        for (let j = i; j < keyword.length + i; j++) {
+            if(result[j] !== undefined){
+                segment += result[j]
+            }
+        }
+        if(segment === keyword){
+            isFound = true
+        }
+        
+    }
+        if(isFound === true){
+            searchResult = [...searchResult, personName]
+        }
+    }
+        cb(searchResult, limit)
 }
 
-searchName('an',3, searchName)
+function limitResult(arr, lim) {
+    let result = []
+    for (let i = 0; i < lim; i++) {
+        if(arr[i]){
+            result = [...result, arr[i]]
+        }
+    }
+    console.log(result)
+}
 
+searchName("an", 10, limitResult)
--- task3.js
+++ task3.js
@@ -1,25 +1,68 @@
-function seleksiNilai(nilaiAwal, nilaiAkhir, dataArray){
-    if (nilaiAwal < nilaiAkhir) {
-        if (dataArray.length > 5) {
-            let hasil = []
-            for (let i = 0; i < dataArray.length; i++) {
-                if (dataArray[i] > nilaiAwal && dataArray[i] < nilaiAkhir){
-                    hasil.push(dataArray[i])
-                    hasil.sort((a,b) => a-b)
-                }
-            }if(hasil.length == 0){
-                return "Nilai tidak ada"
-            }
-            return hasil;
-            // console.log(temp)
-        }
-        else{
-            return "Jumlah angka dalam data array harus lebih dari 5"
-        }
-    }else {
-        return ("Nilai akhir harus lebih besar dari nilai awal")
+const SeleksiNilai = function(nilaiAwal, nilaiAkhir, dataArray) {
+    if (nilaiAwal > nilaiAkhir) {
+        console.log("Nilai akhir harus lebih besar dari nilai awal")
+    }
+    if (dataArray.length < 5) {
+        console.log("Jumlah angka dalam dataArray harus lebih dari 5")
     }
 
+    let result = []
+
+    for (let i = 0; i < dataArray.length; i++) {
+        const num = dataArray[i]
+        if (num > nilaiAwal) {
+            if(num < nilaiAkhir){
+result = [...result, num]
+            }
+        }
+        
+    }
+
+
+    if (result.length === 0) {
+        console.log("Nilai tidak ditemukan")
+    }else{
+        // sorting algorithm
+        quickSort(result, 0, result.length - 1)
+        console.log(result)
+    }
+}    
+
+// sorting algorithm quickSort()
+function quickSort(arr, batasBawah, batasAtas){
+    if(batasBawah < batasAtas){
+        const num = partisi(arr, batasBawah, batasAtas)
+        quickSort(arr, batasBawah, num -1)
+        quickSort(arr, num + 1, batasAtas)
+    }
 }
-const hasil = seleksiNilai(50,20,[2,25,4,14,17,30,8])
-console.log(hasil)
+
+function partisi(arr, batasBawah, batasAtas) {
+    const pembagi = arr[batasAtas];
+    let i = (batasBawah - 1);
+    for (let j = batasBawah; j < batasAtas; j++) {
+        if (arr[j] < pembagi) {
+            i++;
+            [arr[i], arr[j]] = [arr[j], arr[i]];
+        }
+    }
+    [arr[i + 1], arr[batasAtas]] = [arr[batasAtas], arr[i + 1]];
+    return i + 1;
+}
+
+    SeleksiNilai(5,20,[2,25,4,14,17,30,8])
+
+
+// quickSort() merupakan algoritma yang berbasis divide and conquer
+// algoritma ini memilih elemen pivot atau pembagi dari array yang mengatur
+// ulang elemen - elemen sehingga elemen yang lebih kecil dari pembagi
+// berada disebelah kiri, dan elemen yang lebih besar disebelah kanan
+// kenapa saya memilih quickSort(). karena memiliki kompleksitas waktu yang baik, efisien
+// untuk dataset yang besar, implementasi yang efisien untuk kebutuhan kode diatas.
+
+// bubbleSort() adalah salah satu algoritma pengurutan sederhana yang bekerja dengan cara 
+// membandingkan dan menukar elemen-elemen secara berpasangan dalam sebuah array
+// sehingga elemen-elemen yang lebih besar bergerak ke akhir array
+// bubbleSort ini merupakan algoritma pengurutan yang sederhana, namun
+// efisiensinya lebih rendah dari pada sorting algoritma yang lain seperti quickSort()
+// terutama pada dataset yang besar
